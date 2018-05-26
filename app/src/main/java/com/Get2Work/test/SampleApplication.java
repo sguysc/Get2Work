e66ed39b85d8279332f971abd4e0f97b32ddf335
sguysc@gmail.com <Gs1234dc>
2018-05-26 14:43:49 +0300

    Get2Work and database connection

---
+++ app/src/main/java/com/Get2Work/test/SampleApplication.java
@@ -0,0 +1,35 @@
+package com.Get2Work.test;
+
+import android.support.multidex.MultiDexApplication;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.here.mobility.sdk.core.HereMobilitySdk;
+
+
+/**********************************************************
+ * Copyright © 2018 HERE Global B.V. All rights reserved. *
+ **********************************************************/
+public class SampleApplication extends MultiDexApplication {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //HereMobilitySdk initialization must be called in onCreate() method to initialize the SDK.
+        HereMobilitySdk.init(this);
+
+        //Returns whether the current process is the SDK agent process.
+        if (HereMobilitySdk.isHereAgentProcess(this)){
+            return;
+        }
+
+        //put here the rest of you app sdks initialization.
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
