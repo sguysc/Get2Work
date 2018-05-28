35e263fc95cfdf07ffff2439e1510b4b54118904
amihayb <amihayb@yahoo.com>
2018-05-28 22:56:45 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/Get2Work/test/nextCustomer.java
@@ -0,0 +1,13 @@
+package com.Get2Work.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nextCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_customer);
+    }
+}
